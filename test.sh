fda68db1879bca4691d0675d80dacfd28417b6c5
Quan111111 <2330109299@qq.com>
2024-08-26 12:59:01 +0800

    修改脚本使得uuid随机生成，端口可自定义

--- test.sh
+++ test.sh
@@ -15,45 +15,15 @@ elif [ -f /etc/lsb-release ]; then
 elif [ -f /etc/debian_version ]; then
     OS=Debian
     VER=$(cat /etc/debian_version)
-elif [ -f /etc/SuSe-release ]; then
-    ...
-elif [ -f /etc/redhat-release ]; then
-    ...
 else
     OS=$(uname -s)
     VER=$(uname -r)
 fi
 
-# 处理APT锁定问题的函数
-handle_apt_lock() {
-    echo "APT 被其他进程锁定。正在尝试修复..."
-    sudo pkill -9 apt-get
-    sudo pkill -9 dpkg
-    echo "清理锁文件..."
-    sudo rm -f /var/lib/dpkg/lock
-    sudo rm -f /var/lib/apt/lists/lock
-    sudo rm -f /var/cache/apt/archives/lock
-    sudo rm -f /var/lib/dpkg/lock-frontend
-    echo "重新配置软件包..."
-    sudo dpkg --configure -a
-    echo "重试更新和安装..."
-}
-
-# 封装更新和安装操作为一个函数
-update_pkg() {
-    if [[ "$OS" == "Ubuntu" ]] || [[ "$OS" == "Debian" ]]; then
-        sudo apt-get update
-    elif [[ "$OS" == "CentOS Linux" ]] || [[ "$OS" == "Fedora" ]]; then
-        sudo yum update -y
-    else
-        echo "不支持的操作系统"
-        exit 1
-    fi
-}
-
 # 安装 Docker
 install_docker() {
     if [[ "$OS" == "Ubuntu" ]] || [[ "$OS" == "Debian" ]]; then
+        sudo apt-get update
         sudo apt-get install docker.io -y
     elif [[ "$OS" == "CentOS Linux" ]]; then
         sudo yum install -y yum-utils
@@ -73,9 +43,6 @@ install_docker() {
     fi
 }
 
-# 尝试更新
-update_pkg
-
 # 安装 Docker
 install_docker
 
@@ -89,9 +56,11 @@ mkdir -p /root/socks/
 # 配置文件路径
 V2RAY_CONFIG_FILE="/root/v2ray/config.json"
 SOCKS_CONFIG_FILE="/root/socks/config_s.json"
+V2RAY_SHARE_LINK_FILE="/root/v2ray/share_v2ray_base64.txt"
+SOCKS_SHARE_LINK_FILE="/root/v2ray/share_socks_base64.txt"
 
 # 获取 IP 地址
-IP_ADDRESSES=($(ip addr show | grep "inet\b" | awk '{print \$2}' | cut -d/ -f1 | grep -v -E "^127\.|^172\.17\.|^10\."))
+IP_ADDRESSES=($(ip addr show | grep "inet\b" | awk "{print \$2}" | cut -d/ -f1 | grep -v -E "^127\.|^172\.17\.|^10\."))
 
 # 生成 UUID
 generate_uuid() {
@@ -99,168 +68,166 @@ generate_uuid() {
 }
 
 # 初始化 V2Ray 配置
-V2RAY_CONFIG_JSON="{\n    \"inbounds\": [\n"
-INBOUND_TEMPLATE='        {
-            "tag": "in-TAG",
-            "listen": "IP_ADDRESS",
-            "port": 12345,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "UUID"
-                    }
-                ]
-            }
-        }'
-OUTBOUND_TEMPLATE='        {
-            "tag": "out-TAG",
-            "sendThrough": "IP_ADDRESS",
-            "protocol": "freedom"
-        }'
-ROUTE_TEMPLATE='            {
-                "type": "field",
-                "inboundTag": "in-TAG",
-                "outboundTag": "out-TAG"
-            }'
+V2RAY_CONFIG_JSON='{
+  "inbounds": [],
+  "outbounds": [],
+  "routing": {
+    "domainStrategy": "IPOnDemand",
+    "rules": []
+  },
+  "dns": {
+    "servers": ["8.8.8.8", "1.1.1.1"]
+  }
+}'
+
+# 初始化 SOCKS 配置
+SOCKS_CONFIG_JSON='{
+  "inbounds": [],
+  "outbounds": [],
+  "routing": {
+    "rules": []
+  }
+}'
+
+# 清空分享链接文件
+echo "" >"$V2RAY_SHARE_LINK_FILE"
+echo "" >"$SOCKS_SHARE_LINK_FILE"
 
 declare -A UUID_MAP
 
+# 输入 Vmess 和 SOCKS 的端口
+read -p "请输入 Vmess 的端口: " VMESS_PORT
+read -p "请输入 SOCKS 的端口: " SOCKS_PORT
+
+# 生成 UUID
+generate_uuid() {
+    cat /proc/sys/kernel/random/uuid
+}
+
+# 生成 Vmess 分享链接
+generate_vmess_share_link() {
+    local ip=$1
+    local uuid=$2
+    local port=$3
+    local link_json=$(
+        cat <<EOF
+{
+  "v": "2",
+  "ps": "vmess_${ip}",
+  "add": "${ip}",
+  "port": "${port}",
+  "id": "${uuid}",
+  "aid": "0",
+  "net": "tcp",
+  "type": "none",
+  "host": "",
+  "path": "",
+  "tls": ""
+}
+EOF
+    )
+    echo "vmess://$(echo -n "$link_json" | base64 -w 0)" >>"$V2RAY_SHARE_LINK_FILE"
+}
+
+generate_socks_share_link_with_auth() {
+    local ip=$1
+    local port=$2
+    local tag_num=$3
+    local user="user-$tag_num"
+    local pass="pass-$tag_num"
+    local remark="IP$tag_num"
+
+    # 对用户名:密码进行 Base64 编码
+    local up_encoded
+    up_encoded=$(echo -n "${user}:${pass}" | base64)
+
+    # 对备注进行 URL 编码
+    local remark_encoded
+    remark_encoded=$(echo -n "${remark}" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+
+    # 构建并写入 V2Ray Socks 分享链接
+    local socks_link="socks://${up_encoded}@${ip}:${port}#${remark_encoded}"
+    echo "$socks_link" >>"$SOCKS_SHARE_LINK_FILE"
+}
+
 for i in "${!IP_ADDRESSES[@]}"; do
-    TAG_NUM=$(printf "%02d" $((i+1)))
+    TAG_NUM=$(printf "%02d" $((i + 1)))
     UUID=$(generate_uuid)
-    UUID_MAP[${IP_ADDRESSES[$i]}]=$UUID
-    INBOUND_ENTRY=${INBOUND_TEMPLATE//IP_ADDRESS/${IP_ADDRESSES[$i]}}
-    INBOUND_ENTRY=${INBOUND_ENTRY//TAG/$TAG_NUM}
-    INBOUND_ENTRY=${INBOUND_ENTRY//UUID/$UUID}
-    V2RAY_CONFIG_JSON+="$INBOUND_ENTRY"
-    if [ $((i+1)) -lt ${#IP_ADDRESSES[@]} ]; then
-        V2RAY_CONFIG_JSON+=",\n"
-    else
-        V2RAY_CONFIG_JSON+="\n"
-    fi
+
+    # 添加 V2Ray 入站规则
+    V2RAY_CONFIG_JSON=$(echo "$V2RAY_CONFIG_JSON" | jq --arg ip "${IP_ADDRESSES[$i]}" --arg uuid "$UUID" --arg port "$VMESS_PORT" --arg tag "in-$TAG_NUM" '.inbounds += [{
+      tag: $tag,
+      listen: $ip,
+      port: ($port | tonumber),
+      protocol: "vmess",
+      settings: { clients: [{ id: $uuid }] }
+    }]')
+
+    # 添加 V2Ray 出站规则
+    V2RAY_CONFIG_JSON=$(echo "$V2RAY_CONFIG_JSON" | jq --arg ip "${IP_ADDRESSES[$i]}" --arg tag "out-$TAG_NUM" '.outbounds += [{
+      tag: $tag,
+      sendThrough: $ip,
+      protocol: "freedom"
+    }]')
+
+    # 添加路由规则
+    V2RAY_CONFIG_JSON=$(echo "$V2RAY_CONFIG_JSON" | jq --arg inTag "in-$TAG_NUM" --arg outTag "out-$TAG_NUM" '.routing.rules += [{
+      type: "field",
+      inboundTag: [$inTag],
+      outboundTag: $outTag
+    }]')
+
+    # 添加 SOCKS 入站规则
+    SOCKS_CONFIG_JSON=$(echo "$SOCKS_CONFIG_JSON" | jq --arg ip "${IP_ADDRESSES[$i]}" --arg port "$SOCKS_PORT" --arg tag "socks-in-$TAG_NUM" --arg user "user-$TAG_NUM" --arg pass "pass-$TAG_NUM" '.inbounds += [{
+        tag: $tag,
+        protocol: "socks",
+        port: ($port | tonumber),
+        listen: $ip,
+        settings: {
+            auth: "password",
+            accounts: [{ user: $user, pass: $pass }],
+            udp: true,
+            ip: $ip
+    }
+    }]')
+
+    # 添加 SOCKS 出站规则
+    SOCKS_CONFIG_JSON=$(echo "$SOCKS_CONFIG_JSON" | jq --arg ip "${IP_ADDRESSES[$i]}" --arg uuid "$UUID" --arg port "$VMESS_PORT" --arg tag "vmess-out-$TAG_NUM" '.outbounds += [{
+      tag: $tag,
+      sendThrough: $ip,
+      protocol: "vmess",
+      settings: {
+        vnext: [{
+          address: $ip,
+          port: ($port | tonumber),
+          users: [{ id: $uuid, alterId: 0, security: "auto" }]
+        }]
+      }
+    }]')
+
+    # 添加路由规则
+    SOCKS_CONFIG_JSON=$(echo "$SOCKS_CONFIG_JSON" | jq --arg inTag "socks-in-$TAG_NUM" --arg outTag "vmess-out-$TAG_NUM" '.routing.rules += [{
+      type: "field",
+      inboundTag: [$inTag],
+      outboundTag: $outTag
+    }]')
+
+    # 生成 Vmess 分享链接
+    generate_vmess_share_link "${IP_ADDRESSES[$i]}" "$UUID" "$VMESS_PORT"
+
+    # 生成 SOCKS 分享链接
+    generate_socks_share_link_with_auth "${IP_ADDRESSES[$i]}" "$SOCKS_PORT" "$TAG_NUM"
 done
 
-V2RAY_CONFIG_JSON+="    ],\n    \"outbounds\": [\n"
-
-for i in "${!IP_ADDRESSES[@]}"; do
-    TAG_NUM=$(printf "%02d" $((i+1)))
-    OUTBOUND_ENTRY=${OUTBOUND_TEMPLATE//IP_ADDRESS/${IP_ADDRESSES[$i]}}
-    OUTBOUND_ENTRY=${OUTBOUND_ENTRY//TAG/$TAG_NUM}
-    V2RAY_CONFIG_JSON+="$OUTBOUND_ENTRY"
-    if [ $i -lt $((${#IP_ADDRESSES[@]} - 1)) ]; then
-        V2RAY_CONFIG_JSON+=",\n"
-    fi
-done
-
-V2RAY_CONFIG_JSON+="\n    ],\n    \"routing\": {\n        \"domainStrategy\": \"IPOnDemand\",\n        \"rules\": [\n"
-
-for i in "${!IP_ADDRESSES[@]}"; do
-    TAG_NUM=$(printf "%02d" $((i+1)))
-    ROUTE_ENTRY=${ROUTE_TEMPLATE//TAG/$TAG_NUM}
-    V2RAY_CONFIG_JSON+="$ROUTE_ENTRY"
-    if [ $i -lt $((${#IP_ADDRESSES[@]} - 1)) ]; then
-        V2RAY_CONFIG_JSON+=",\n"
-    fi
-done
-
-V2RAY_CONFIG_JSON+="\n        ]\n    },\n    \"dns\": {\n        \"servers\": [\n            \"8.8.8.8\",\n            \"1.1.1.1\"\n        ]\n    }\n}"
-V2RAY_CONFIG_JSON+="\n}"
-
-echo -e "$V2RAY_CONFIG_JSON" > "$V2RAY_CONFIG_FILE"
+echo "$V2RAY_CONFIG_JSON" | jq '.' >"$V2RAY_CONFIG_FILE"
 echo "V2Ray 配置文件已创建在 $V2RAY_CONFIG_FILE"
 
-# 初始化 SOCKS 配置
-SOCKS_CONFIG_JSON="{\n    \"inbounds\": [\n"
-SOCKS_PORT=18200
-SOCKS_INBOUND_TEMPLATE='        {
-            "tag": "socks-in-TAG",
-            "protocol": "socks",
-            "port": PORT_NUM,
-            "listen": "IP_ADDRESS",
-            "settings": {
-                "auth": "password",
-                "accounts": [
-                    {
-                        "user": "user-TAG",
-                        "pass": "pass-TAG"
-                    }
-                ],
-                "udp": true,
-                "ip": "IP_ADDRESS",
-                "userLevel": 0
-            }
-        }'
-SOCKS_OUTBOUND_TEMPLATE='        {
-            "tag": "vmess-out-TAG",
-            "sendThrough": "IP_ADDRESS",
-            "protocol": "vmess",
-            "settings": {
-                "vnext": [
-                    {
-                        "address": "IP_ADDRESS",
-                        "port": 12345,
-                        "users": [
-                            {
-                                "id": "UUID",
-                                "alterId": 0,
-                                "security": "auto"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }'
-SOCKS_ROUTE_TEMPLATE='            {
-                "type": "field",
-                "inboundTag": "socks-in-TAG",
-                "outboundTag": "vmess-out-TAG"
-            }'
-
-for i in "${!IP_ADDRESSES[@]}"; do
-    TAG_NUM=$(printf "%02d" $((i+1)))
-    PORT_NUM=$((${SOCKS_PORT} + i))
-    UUID=${UUID_MAP[${IP_ADDRESSES[$i]}]}
-    SOCKS_INBOUND_ENTRY=${SOCKS_INBOUND_TEMPLATE//IP_ADDRESS/${IP_ADDRESSES[$i]}}
-    SOCKS_INBOUND_ENTRY=${SOCKS_INBOUND_ENTRY//TAG/$TAG_NUM}
-    SOCKS_INBOUND_ENTRY=${SOCKS_INBOUND_ENTRY//PORT_NUM/$PORT_NUM}
-    SOCKS_CONFIG_JSON+="$SOCKS_INBOUND_ENTRY"
-    if [ $((i+1)) -lt ${#IP_ADDRESSES[@]} ]; then
-        SOCKS_CONFIG_JSON+=",\n"
-    else
-        SOCKS_CONFIG_JSON+="\n"
-    fi
-done
-
-SOCKS_CONFIG_JSON+="    ],\n    \"outbounds\": [\n"
-
-for i in "${!IP_ADDRESSES[@]}"; do
-    TAG_NUM=$(printf "%02d" $((i+1)))
-    UUID=${UUID_MAP[${IP_ADDRESSES[$i]}]}
-    SOCKS_OUTBOUND_ENTRY=${SOCKS_OUTBOUND_TEMPLATE//IP_ADDRESS/${IP_ADDRESSES[$i]}}
-    SOCKS_OUTBOUND_ENTRY=${SOCKS_OUTBOUND_ENTRY//TAG/$TAG_NUM}
-    SOCKS_OUTBOUND_ENTRY=${SOCKS_OUTBOUND_ENTRY//UUID/$UUID}
-    SOCKS_CONFIG_JSON+="$SOCKS_OUTBOUND_ENTRY"
-    if [ $i -lt $((${#IP_ADDRESSES[@]} - 1)) ]; then
-        SOCKS_CONFIG_JSON+=",\n"
-    fi
-done
-
-SOCKS_CONFIG_JSON+="\n    ],\n    \"routing\": {\n        \"rules\": [\n"
-
-for i in "${!IP_ADDRESSES[@]}"; do
-    TAG_NUM=$(printf "%02d" $((i+1)))
-    SOCKS_ROUTE_ENTRY=${SOCKS_ROUTE_TEMPLATE//TAG/$TAG_NUM}
-    SOCKS_CONFIG_JSON+="$SOCKS_ROUTE_ENTRY"
-    if [ $i -lt $((${#IP_ADDRESSES[@]} - 1)) ]; then
-        SOCKS_CONFIG_JSON+=",\n"
-    fi
-done
-
-SOCKS_CONFIG_JSON+="\n        ]\n    }\n}"
-
-echo -e "$SOCKS_CONFIG_JSON" > "$SOCKS_CONFIG_FILE"
+echo "$SOCKS_CONFIG_JSON" | jq '.' >"$SOCKS_CONFIG_FILE"
 echo "SOCKS 配置文件已创建在 $SOCKS_CONFIG_FILE"
+
+echo "V2Ray分享链接已创建在 $V2RAY_SHARE_LINK_FILE"
+
+echo "SOCKS分享链接已创建在 $SOCKS_SHARE_LINK_FILE"
 
 # 启动 V2Ray 容器
 if [ $(docker ps -a -q -f name=test_v) ]; then
@@ -291,7 +258,10 @@ add_to_startup() {
     if crontab -l | grep -Fq "/bin/bash $script_path"; then
         echo "脚本 $script_path 已经在开机启动中了，不会重复添加。"
     else
-        (crontab -l 2>/dev/null; echo "$cron_entry") | crontab -
+        (
+            crontab -l 2>/dev/null
+            echo "$cron_entry"
+        ) | crontab -
         echo "脚本 $script_path 已添加到开机启动，将在启动后60秒执行。"
     fi
 }
